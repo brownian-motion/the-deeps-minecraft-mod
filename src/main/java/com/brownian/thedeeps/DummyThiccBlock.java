fe8dab8dec10cff9163a839f13f3bd7986a8c228
JJ Brown <jack.brown.255@gmail.com>
2021-06-18 21:33:08 -0500

    First commit -- making a dummy (thicc) block!

---
+++ src/main/java/com/brownian/thedeeps/DummyThiccBlock.java
@@ -0,0 +1,11 @@
+package com.brownian.thedeeps;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+
+class DummyThiccBlock extends Block {
+    public DummyThiccBlock() {
+        super(Properties.of(Material.STONE, MaterialColor.COLOR_LIGHT_BLUE));
+    }
+}
---
+++ src/main/java/com/brownian/thedeeps/TheDeepsBlocks.java
@@ -0,0 +1,16 @@
+package com.brownian.thedeeps;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class TheDeepsBlocks {
+    static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TheDeepsMod.MODID);
+    static final RegistryObject<Block> DUMMY_BLOCK = BLOCKS.register("thicc", DummyThiccBlock::new);
+
+//    @SubscribeEvent
+//    public static void registerItemblocks(RegistryEvent.Register<Item> evt) {
+//        TheDeepsBlockItems.registerBlockItems(evt);
+//    }
+}
