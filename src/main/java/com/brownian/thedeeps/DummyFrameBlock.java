249b944c64342db80d8f829b914ae779da6c08d1
Flame <osageman70@gmail.com>
2021-06-20 23:33:09 -0500

    Added dummy frame block

---
+++ src/main/java/com/brownian/thedeeps/DummyFrameBlock.java
@@ -0,0 +1,10 @@
+package com.brownian.thedeeps;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+
+class DummyFrameBlock extends Block{
+    public DummyFrameBlock() {super(Properties.of(Material.STONE, MaterialColor.COLOR_GRAY));
+}
+}
--- src/main/java/com/brownian/thedeeps/TheDeepsBlocks.java
+++ src/main/java/com/brownian/thedeeps/TheDeepsBlocks.java
@@ -8,7 +8,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class TheDeepsBlocks {
     static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TheDeepsMod.MODID);
     static final RegistryObject<Block> DUMMY_BLOCK = BLOCKS.register("thicc", DummyThiccBlock::new);
-
+    static final RegistryObject<Block> Frame_Block = BLOCKS.register("frame", DummyFrameBlock::new);
 //    @SubscribeEvent
 //    public static void registerItemblocks(RegistryEvent.Register<Item> evt) {
 //        TheDeepsBlockItems.registerBlockItems(evt);
