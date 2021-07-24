493209fbc6caf6720a714b970e66b361d867ccf4
JJ Brown <jack.brown.255@gmail.com>
2021-07-23 21:28:01 -0500

    Added the loot table for the dummy thicc block.
    We have not added loot tables for anything else! Consider this the "Hello world!" of dropping items.

--- src/main/java/com/brownian/thedeeps/TheDeepsBlocks.java
+++ src/main/java/com/brownian/thedeeps/TheDeepsBlocks.java
@@ -7,10 +7,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class TheDeepsBlocks {
     static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TheDeepsMod.MODID);
+    //TODO: make a @DropsItem annotation which generates the loot_table for us!
     static final RegistryObject<Block> DUMMY_BLOCK = BLOCKS.register("thicc", DummyThiccBlock::new);
     static final RegistryObject<Block> Frame_Block = BLOCKS.register("frame", DummyFrameBlock::new);
-//    @SubscribeEvent
-//    public static void registerItemblocks(RegistryEvent.Register<Item> evt) {
-//        TheDeepsBlockItems.registerBlockItems(evt);
-//    }
 }
