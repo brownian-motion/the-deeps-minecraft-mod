b01cfab32e808f90435c2389ac8474d6d3d0ddf8
JJ Brown <jack.brown.255@gmail.com>
2021-07-25 12:37:23 -0500

    Added an item to create a portal from the bottom of the frame
    Signed-off-by: JJ Brown <jack.brown.255@gmail.com>

--- src/main/java/com/brownian/thedeeps/TheDeepsBlockItems.java
+++ src/main/java/com/brownian/thedeeps/TheDeepsBlockItems.java
@@ -1,5 +1,6 @@
 package com.brownian.thedeeps;
 
+import com.brownian.thedeeps.items.PortalCatalystItem;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -11,12 +12,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class TheDeepsBlockItems {
     static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TheDeepsMod.MODID);
 
-    private static final RegistryObject<Item> DUMMY_BLOCK_ITEM = blockItem("block/thicc", TheDeepsBlocks.DUMMY_BLOCK);
-    private static final RegistryObject<Item> Dummy_FRAME_ITEM = blockItem("block/frame", TheDeepsBlocks.Frame_Block);
-    private static final RegistryObject<Item> DUMMY_PORTAL_ITEM = blockItem("block/deeps_portal", TheDeepsBlocks.DUMMY_PORTAL);
+    private static final RegistryObject<BlockItem> DUMMY_BLOCK_ITEM = blockItem("block/thicc", TheDeepsBlocks.DUMMY_BLOCK);
+    private static final RegistryObject<BlockItem> Dummy_FRAME_ITEM = blockItem("block/frame", TheDeepsBlocks.Frame_Block);
+    private static final RegistryObject<BlockItem> DUMMY_PORTAL_ITEM = blockItem("block/deeps_portal", TheDeepsBlocks.DUMMY_PORTAL);
 
-    private static RegistryObject<Item> blockItem(String name, RegistryObject<Block> block){
+    private static final RegistryObject<PortalCatalystItem> PORTAL_CATALYST_ITEM = ITEMS.register("tool/portal_catalyst", PortalCatalystItem::new);
+
+    private static <B extends Block> RegistryObject<BlockItem> blockItem(String name, RegistryObject<B> block) {
         return TheDeepsBlockItems.ITEMS.register(name, () -> new BlockItem(block.get(), new BlockItem.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS)));
     }
-
 }
