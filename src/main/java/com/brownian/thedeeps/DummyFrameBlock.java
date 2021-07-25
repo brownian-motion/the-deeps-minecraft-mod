294587ed8603c441f15446984b7cf50d0d5a8fa3
Flame <osageman70@gmail.com>
2021-07-24 19:54:43 -0500

    Added Block Hardness

--- src/main/java/com/brownian/thedeeps/DummyFrameBlock.java
+++ src/main/java/com/brownian/thedeeps/DummyFrameBlock.java
@@ -1,10 +1,10 @@
 package com.brownian.thedeeps;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.material.MaterialColor;
+import net.minecraft.block.Blocks;
 
-class DummyFrameBlock extends Block{
-    public DummyFrameBlock() {super(Properties.of(Material.STONE, MaterialColor.COLOR_GRAY));
-}
+class DummyFrameBlock extends Block {
+    public DummyFrameBlock() {
+        super(Properties.copy(Blocks.OBSIDIAN));
+    }
 }
